8d9f2e5647ad4010fe952794a2915fc6a8716141
Satyam Naik <satyamnaik15@gmail.com>
2020-10-27 15:14:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header >
+       <a href="/">React shoping cart</a>
+      </header>
+      <main>
+        product
+      </main>
+      <footer>
+        All right reserved  .
+      </footer>
+    </div>
+  );
+}
+
+export default App;
